40c60a3e1b1edf5edbe5f9a9f138091d4daa4234
Roman Rybachok <ribachok.2020@gmail.com>
2025-04-27 22:45:54 +0300

    lab 4 auth and db

--- lab4/src/components/header/Header.js
+++ lab4/src/components/header/Header.js
@@ -1,17 +1,37 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/authContext';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
 import "./Header.css";
 
 const Header = () => {
+  const { userLoggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth); // Вихід з Firebase
+      navigate('/'); // Перенаправлення на головну сторінку
+    } catch (error) {
+      console.log('Sign out error:', error.message);
+    }
+  };
+
   return (
     <nav>
       <div className="title">
         <h1>УБМ</h1>
       </div>
-      <ul className="nav-menu">
-        <li><Link to="/myCity">Моє місто</Link></li> 
-        <li><Link to="/myCity#construction">Будівництво</Link></li>
-        <li><Link to="/resources">Ресурси міста</Link></li>
-      </ul>
+
+      {userLoggedIn && ( // Показувати меню тільки якщо користувач авторизований
+        <ul className="nav-menu">
+          <li><Link to="/myCity">Моє місто</Link></li>
+          <li><Link to="/myCity#construction">Будівництво</Link></li>
+          <li><Link to="/resources">Ресурси міста</Link></li>
+          <li><button className="sign-out-btn" onClick={handleSignOut}>Вихід</button></li>
+        </ul>
+      )}
     </nav>
   );
 };
--- lab4/src/context/cityContext.js
+++ lab4/src/context/cityContext.js
@@ -1,4 +1,8 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { app } from '../firebase/firebase'
+
+const db = getFirestore(app); // Ініціалізація Firestore
 
 const CityContext = createContext();
 
@@ -14,24 +18,41 @@ export const CityProvider = ({ children }) => {
   const [typeImages, setTypeImages] = useState({});
   const [resources, setResources] = useState({});
 
-  useEffect(() => {
-    const loadResources = async () => {
-      try {
-        const response = await fetch('data.json');
-        const data = await response.json();
+  // Функція для отримання даних з Firestore
+  const loadResources = async () => {
+    try {
+      const totalCityResourcesRef = doc(db, "UBMdatabase", "total_city_resources");
+      const imagesLinksRef = doc(db, "UBMdatabase", "images_links");
+      const needResourcesRef = doc(db, "UBMdatabase", "need_resources");
+  
+      const totalCityResourcesSnap = await getDoc(totalCityResourcesRef);
+      const imagesLinksSnap = await getDoc(imagesLinksRef);
+      const needResourcesSnap = await getDoc(needResourcesRef);
+  
+      if (totalCityResourcesSnap.exists() && imagesLinksSnap.exists() && needResourcesSnap.exists()) {
+        const totalCityData = totalCityResourcesSnap.data();
+        const imagesData = imagesLinksSnap.data();
+        const resourcesData = needResourcesSnap.data();
+  
+        console.log("Завантажені зображення:", imagesData);
+        console.log("Ключі typeImages:", Object.keys(imagesData)); // Лог для перевірки ключів
 
-        setBudget(data.budget);
-        setMaterials(data.materials);
-        setWorkers(data.workers);
-        setTypeImages(data.typeImages);
-        setResources(data.resources);
 
-      } catch (error) {
-        console.error('Помилка при завантаженні data.json:', error);
+        setBudget(totalCityData.budget);
+        setMaterials(totalCityData.materials);
+        setWorkers(totalCityData.workers);
+        setTypeImages(imagesData);
+        setResources(resourcesData);
+      } else {
+        console.log("Документи не знайдено");
       }
-    };
+    } catch (error) {
+      console.error('Помилка при завантаженні даних з Firestore:', error);
+    }
+  };  
 
-    loadResources();
+  useEffect(() => {
+    loadResources(); // Завантажуємо дані з Firestore під час завантаження компонента
   }, []);
 
   const updateResources = (newMaterials, newBudget, newWorkers) => {
